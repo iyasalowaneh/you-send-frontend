5dca9991c6a79335650406fd0080bdfe5753dc5c
iyasalowaneh <iyas_moh@yahoo.com>
2021-08-03 23:09:36 +0300

    added use effect

--- src/components/ChatItem.js
+++ src/components/ChatItem.js
@@ -1,18 +1,25 @@
-import { useReducer } from "react";
 import "../Chat.css";
 const ChatItem = (props) => {
   const message = props.message;
   const userImage = props.userImage;
   const user = props.user;
+
+
+
+  // const el = useRef(null);
+
+  // useEffect(() => {
+  //   el.current.scrollIntoView({behavior: "smooth", block: "end", inline: "nearest"});
+  // });<div id={"el"} ref={el} >
+
+
+
+
   return (
     <>
-      {user.id !== message.senderId && (
-        <div class="heading-avatar-icon">
-          <p class="msg_cotainer"> {message.content}</p>
-        </div>
-      )}
+      
 
-      {user.id === message.senderId && (
+      {user.id === message.senderId && ( 
         <div class="d-flex justify-content-end mb-4">
           <p class="msg_cotainer_send">
             {message.content}
@@ -20,6 +27,11 @@ const ChatItem = (props) => {
           </p>
           <img src={message.image} />
           <img class="rounded-circle user_img_msg" src={userImage} />
+        </div>
+      )}
+      {user.id !== message.senderId && ( 
+        <div class="heading-avatar-icon">
+          <p class="msg_cotainer"> {message.content}</p>
         </div>
       )}
     </>
