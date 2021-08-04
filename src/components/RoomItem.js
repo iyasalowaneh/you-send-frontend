3d3c1501c28f309e64405cb4a6155b2de2c21f55
Abdullah <abdallah.b.b96@gmail.com>
2021-08-04 12:11:51 +0300

    last

--- src/components/RoomItem.js
+++ src/components/RoomItem.js
@@ -25,17 +25,6 @@ const RoomItem = ({ setUserId, user, setRoomId   }) => {
         </li>
       </ui>
 
-      {/*  
-<div class="d-flex justify-content-end mb-4">
-<p class="msg_cotainer_send">
-{user.name}
-  <p class="msg_time_send">8:55 AM, Today</p>
-</p>
-<img
-  class="rounded-circle user_img_msg"
-  src="https://static.turbosquid.com/Preview/001292/481/WV/_D.jpg"
-/>
-</div> */}
     </>
   );
 };
